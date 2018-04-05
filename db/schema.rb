186d4dadbcda463d2dbd06b333322336fbfff42c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-04-05 18:09:02 -0300

    Update user model, create tweet model and votable migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405200243) do
+ActiveRecord::Schema.define(version: 20180405210218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20180405200243) do
     t.index ["follower_id", "follower_type"], name: "fk_follows"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.integer "tweet_original_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -35,4 +44,19 @@ ActiveRecord::Schema.define(version: 20180405200243) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+  end
+
+  add_foreign_key "tweets", "users"
 end
