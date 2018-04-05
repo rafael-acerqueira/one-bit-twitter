c57585d5d8b454e9b8a38f85a3431bb313527f5b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-04-05 17:31:40 -0300

    Add user and include authentication with JWT

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,7 @@
 module Api
    module V1
-      class ApiController < ApplicationController        
+      class ApiController < ApplicationController
+        include Knock::Authenticable 
       end
    end
 end
