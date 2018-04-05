93031625a1ab9583c41620734d9167cc24fd12d9
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-04-05 18:21:03 -0300

    Create rules with cancancan gem

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,8 @@
 module Api
    module V1
       class ApiController < ApplicationController
-        include Knock::Authenticable 
+        include Knock::Authenticable
+        include CanCan::ControllerAdditions
       end
    end
 end
