f900ccf75a69cf7cfe698c7403112333ead436df
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-04-20 19:40:19 -0300

    Add timeline method and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,13 @@ class User < ApplicationRecord
   acts_as_voter
   acts_as_followable
   acts_as_follower
-  has_many :tweets, dependent: :destroy  
+  has_many :tweets, dependent: :destroy
+
+  def timeline
+    timeline = tweets.map { |tweet| tweet }
+    all_following.each do |user|
+      timeline += user.tweets.map { |tweet| tweet }
+    end
+    timeline.sort_by!(&:created_at).reverse
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'timeline', to: 'timeline#index'
       post 'user_token', to: 'user_token#create'
       resources :tweets, only: %i[index show create update destroy] do
         member do
