f7d6321fc57e2291c16fae301184b665c2511146
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-04-27 19:16:43 -0300

    Add request test to likes controller

--- spec/requests/api/v1/api_v1_likes_spec.rb
+++ spec/requests/api/v1/api_v1_likes_spec.rb
@@ -1,10 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Likes", type: :request do
-  describe "GET /api_v1_likes" do
-    it "works! (now write some real specs)" do
-      get api_v1_likes_index_path
-      expect(response).to have_http_status(200)
+  describe 'POST /api/v1/tweets/:id/like' do
+
+    context 'Authenticated' do
+      let(:user) { create(:user) }
+      let(:tweet) { create(:tweet) }
+
+      it 'return created' do
+        post "/api/v1/tweets/#{tweet.id}/like", headers: header_with_authentication(user)
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'tweet was liked by user' do
+        post "/api/v1/tweets/#{tweet.id}/like", headers: header_with_authentication(user)
+        expect(tweet.liked_by user).to be_truthy
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/tweets/:id/like' do
+
+    context 'Authenticated' do
+
+      let(:user) { create(:user) }
+      let(:tweet) { create(:tweet) }
+
+      it do
+        delete "/api/v1/tweets/#{tweet.id}/like", headers: header_with_authentication(user)
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'tweet was unliked by user' do
+        delete "/api/v1/tweets/#{tweet.id}/like", headers: header_with_authentication(user)
+        expect(tweet.unliked_by user).to be_truthy
+      end
     end
   end
 end
