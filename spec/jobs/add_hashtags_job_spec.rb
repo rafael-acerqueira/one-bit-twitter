0e7e3f8e3cba3361186774674d06bc5003bd598b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-05-02 20:01:32 -0300

    Test if jobs are in the correct queue

--- spec/jobs/add_hashtags_job_spec.rb
+++ spec/jobs/add_hashtags_job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AddHashtagsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do
+    it "check if job was enqueued" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        AddHashtagsJob.perform_later('tweet_body')
+      }.to have_enqueued_job
+    end
+
+    it 'is in trendings queue' do
+      expect(AddHashtagsJob.new.queue_name).to eq('trendings')
+    end
+  end
 end
--- spec/jobs/update_trendings_job_spec.rb
+++ spec/jobs/update_trendings_job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UpdateTrendingsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do
+    it "check if job was enqueued" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        UpdateTrendingsJob.perform_later
+      }.to have_enqueued_job
+    end
+
+    it 'is in trendings queue' do
+      expect(UpdateTrendingsJob.new.queue_name).to eq('trendings')
+    end  
+  end
 end
